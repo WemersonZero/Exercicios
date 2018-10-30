68cacdc4ef623817d49731ec30ae111c88a8a7ca
Zero <wemersonjsa@gmail.com>
2018-10-30 16:12:13 -0300

    Adicionado novos arquivos ao repositório

---
+++ ex006.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número: '))
+d = n * 2
+
+print('O dobro de {} vale {}.'.format(n, n*2))
+print('Otriplo de {} vale {}. \nA raiz quadrada de {} e igual a {:.2f}.'.format(n, (n*3), n, pow(n,(1/2))))
---
+++ ex008.py
@@ -0,0 +1,2 @@
+medida = float(input('Uma diistancia em metros: '))
+print('Amedia de {}MT corresponde a \n{}km \n{}hm '.format(medida, medida/1000, medida*1000,))
