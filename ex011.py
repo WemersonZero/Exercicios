bf9c813c404318e7603ce94da8d43f21a911531a
Zero <wemersonjsa@gmail.com>
2018-11-05 20:12:43 -0300

    Adicionado novos arquivos ao repositório

--- ex011.py
+++ ex011.py
@@ -1,4 +1,6 @@
 larg = float(input('Largura da parede: '))
 alt = float(input('Altura da parede: '))
-print('Sua parede tem a dimenção de {}x{} e a sua área é de {:.3f}'.format(larg, alt, (larg**alt)))
-print('Para pinta essa parede, você precisará de {:.4f}L de tinta.'.format((larg**alt)/2))
+área = larg * alt
+print('Sua parede tem a dimenção de {}x{} e a sua área é de {:.1f}m²'.format(larg, alt, área))
+tinta = área / 2
+print('Para pinta essa parede, você precisará de {:.1f}L de tinta.'.format(tinta))
